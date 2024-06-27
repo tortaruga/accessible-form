1b265d94c88922ad6878e17604716952545ae5a2
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-27 15:33:37 +0200

    updated html, css, js files, and README:md

--- app.js
+++ app.js
@@ -1,123 +1,110 @@
-const inputElements = document.querySelectorAll('input');
-const textarea = document.querySelector('textarea');
-const radioBtns = document.querySelectorAll('input[type="radio"]');
-
-
 const form = document.querySelector('form');
-
-const firstNameError = document.getElementById('first-name-error');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email-address');
+const radioBtns = document.querySelectorAll('input[type="radio"]');
+const messageTxtArea = document.getElementById('message');
+const consent = document.getElementById('consent');
 const errorMessages = document.querySelectorAll('.error-message');
-
-let isRadioChecked;
-
-
-const successDialog = document.querySelector('dialog')
+const dialog = document.querySelector('dialog');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    validateForm();
-    console.log('koroshite kudasai')
-    
-    // successDialog.show()
-   
+  e.preventDefault();
+  validateForm();
 })
 
 function validateForm() {
-  inputElements.forEach(input => {
 
-if (input.id === 'first-name') {
-    console.log(input.id);
-    validateName(input);
-    handleErrorMsg(input);
-} else if (input.id === 'last-name') {
-    validateName(input);
-    console.log(input.id);
-    handleErrorMsg(input);
-} else if (input.id === 'email-address') {
-    console.log(input.id);
-    validateEmail(input);
-    handleErrorMsg(input); 
-} else if (input.id === 'consent') {
-    console.log(input.id);
-    isChecked(input);
-    handleErrorMsg(input); 
-}
+  callValidatingFunctions();
 
-  })
-
-  if (!textarea.value) {
-    textarea.ariaInvalid = 'true';
-  } else {
-    textarea.ariaInvalid = 'false';
-  }
-  handleErrorMsg(textarea);
-  console.log(textarea.ariaInvalid)
-
-
-  const isRadioChecked = (btn) => btn.checked; 
-
-  if (!(Array.from(radioBtns).some(isRadioChecked))) {
-    radioBtns[0].ariaInvalid = 'true';    
-  } else {
-    radioBtns[0].ariaInvalid = 'false';
-  }
-  handleErrorMsg(radioBtns[0]);
-  console.log(radioBtns[0].ariaInvalid)
-
-  if (Array.from(inputElements).every(input => input.ariaInvalid === 'false') && textarea.ariaInvalid === 'false' && radioBtns[0].ariaInvalid === 'false') {
-        inputElements.forEach(input => console.log(input.ariaInvalid))
-        successDialog.show();
-      }
-}
-
-
-function validateName(input) {
-  if (!input.value) { 
-        input.ariaInvalid = 'true';
-  } else {
-    input.ariaInvalid = 'false';
-  }
-  console.log(input.ariaInvalid)
-
-}
-
-
-function validateEmail(input) {
-    let emailPattern = /^[A-Za-z0-9-.]+@[a-z]+.[a-z]{2,}$/;
-    if (!emailPattern.test(input.value)) {
-        input.ariaInvalid = 'true';
-    } else {
-        input.ariaInvalid = 'false';
+if (!isEmpty(firstName) &&
+    !isEmpty(lastName) && 
+    !isEmpty(messageTxtArea) &&
+    isEmailValid(email) && 
+    isChecked(consent) &&
+    isOneRadioChecked(Array.from(radioBtns))) {
+      dialog.show();
+    }  else {
+      dialog.close()
     }
-    console.log(input.ariaInvalid)
+}
 
+function callValidatingFunctions() {
+  isEmpty(firstName);
+  isEmpty(lastName);
+  isEmpty(messageTxtArea) 
+  isEmailValid(email);
+  isChecked(consent);
+  isOneRadioChecked(Array.from(radioBtns));
+}
+
+function isEmpty(input) {
+   if (!input.value) {
+    input.ariaInvalid = 'true'; 
+    handleErrorMsg(input);
+    return true;
+   } else {
+    input.ariaInvalid = 'false'; 
+    handleErrorMsg(input);
+    return false;
+   }
+}
+
+function isEmailValid(input) {
+  const emailPattern = /^[A-Za-z0-9.-]+@[a-z]+.[a-z]{2,}$/;
+  if (emailPattern.test(input.value)) {
+    input.ariaInvalid = 'false';
+    handleErrorMsg(input);
+    return true;
+  } else {
+    input.ariaInvalid = 'true'; 
+    handleErrorMsg(input);
+    return false;
+  }
 }
 
 function isChecked(input) {
-   if (!input.checked) {
-    input.ariaInvalid = 'true';
-    console.log(input.ariaInvalid);
-   } else {
+  if (input.checked) {
     input.ariaInvalid = 'false';
-   }
-   console.log(input.ariaInvalid)
-   
+    handleErrorMsg(input);
+    return true;
+  } else {
+    input.ariaInvalid = 'true'; 
+    handleErrorMsg(input);
+    return false;
+  }
+}
+
+function isOneRadioChecked(radios) {
+ if (radios.some(isChecked)) {
+    return true;
+ } else {
+  return false;
+ }
 }
 
 function handleErrorMsg(input) {
   if (input.ariaInvalid === 'true') {
-     errorMessages.forEach(msg => {
-        if (msg.getAttribute('id') === input.getAttribute('aria-describedby')) {
-            msg.classList.remove('hidden');
-            input.classList.add('invalid')
-        }
-     })
-  }  else {
-    errorMessages.forEach(msg => {
-        if (msg.getAttribute('id') === input.getAttribute('aria-describedby')) {
-            msg.classList.add('hidden');
-            input.classList.remove('invalid')
-        }
+    showError(input);
+  } else {
+    hideError(input);
+  }
+}
+
+function showError(input) {
+  input.classList.add('invalid');
+  errorMessages.forEach(msg => {
+    if (msg.id === input.getAttribute('aria-describedby')) {
+      msg.classList.remove('hidden');
+    }
   })
- }
+}
+
+function hideError(input) {
+  input.classList.remove('invalid');
+errorMessages.forEach(msg => {
+  if (msg.id === input.getAttribute('aria-describedby')) {
+    msg.classList.add('hidden');
+  }
+ })
 }
